47bb49174282de48f9ddd9c72ffa40f3c52dd2d8
Giuseppe Giunta <pino.powergiunta@gmail.com>
2020-03-17 09:45:21 +0100

    first commit
    First Commit

---
+++ StateMachineGP1/Assets/DetecManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Hitzone { figlio1, figlio2 }
+
+public class DetecManager : MonoBehaviour
+{
+
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void DetectedCollision(Hitzone zone)
+    {
+        Debug.Log(zone);
+        switch (zone)
+        {
+            case Hitzone.figlio1:
+
+                
+
+                break;
+            case Hitzone.figlio2:
+
+               
+
+                break;
+            default:
+                break;
+        }
+
+    }
+}
---
+++ StateMachineGP1/Assets/Detective.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detective : MonoBehaviour
+{
+    DetecManager manager;
+    public Hitzone myZone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = transform.parent.GetComponentInParent<DetecManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("collisione");
+        manager.DetectedCollision(myZone);
+    }
+}
